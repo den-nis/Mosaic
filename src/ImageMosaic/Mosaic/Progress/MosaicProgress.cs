ddcd6baaa64f4e8a12dc0dea72b40b263e32cdd4
den-nis <50838791+den-nis@users.noreply.github.com>
2021-07-27 20:47:08 +0200

    Progress + csproj

---
+++ src/ImageMosaic/Mosaic/Progress/MosaicProgress.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mosaic.Progress
+{
+	public record MosaicProgress
+	{
+		public MosaicProgress(string name, float percentage)
+		{
+			Name = name;
+			Percentage = percentage;
+		}
+
+		public MosaicProgress(string name)
+		{
+			Name = name;
+			Percentage = -1f;
+		}
+
+		public string Name { get; set; }
+		public float Percentage { get; set; }
+
+		public override string ToString()
+		{
+			if (Percentage != -1)
+			{
+				return $"{Name} - {Math.Round(Percentage * 100, 2)}%";
+			}
+			else
+			{
+				return $"{Name}...";
+			}
+		}
+	}
+}
+
