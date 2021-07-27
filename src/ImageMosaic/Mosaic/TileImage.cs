808181e1aa61d36230615857f051ae00dcc03c3a
den-nis <50838791+den-nis@users.noreply.github.com>
2021-07-27 20:16:44 +0200

    Added tile image

---
+++ src/ImageMosaic/Mosaic/TileImage.cs
@@ -0,0 +1,57 @@
+﻿using ImageMagick;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mosaic
+{
+	public class TileImage : IDisposable
+	{
+		public TileImageMode TileMode { get; set; } = TileImageMode.Center;
+		public MagickColor Color { get; private set; }
+
+		private readonly string _imagePath;
+
+		public TileImage(string imagePath)
+		{
+			_imagePath = imagePath;
+		}
+
+		public MagickImage Thumbnail { get; private set; }
+		public MagickImage Image { get; private set; }
+
+		public void Initialize(int renderResolution, int thumbnailResolution)
+		{
+			Image = Render(renderResolution);
+			Thumbnail = Render(thumbnailResolution);
+			
+			ComputeAverageColor(Image);
+		}
+
+		private MagickImage Render(int resolution)
+		{
+			var image = new MagickImage(_imagePath);
+			ToSquare(image, resolution);
+			return image;
+		}
+
+		private void ToSquare(MagickImage target, int size)
+		{
+			target.ConvertToSquare(TileMode);
+			target.Resize(size, size);
+		}
+
+		private void ComputeAverageColor(MagickImage image)
+		{
+			Color = image.GetAverageColor();
+		}
+
+		public void Dispose()
+		{
+			Image.Dispose();
+			GC.SuppressFinalize(this);
+		}
+	}
+}
