e84b5167a34ecf64fc6cf50b652b2697334cc1fb
den-nis <50838791+den-nis@users.noreply.github.com>
2021-08-02 23:22:02 +0200

    UI Update

--- src/ImageMosaic/Mosaic.UI/MainWindow.xaml.cs
+++ src/ImageMosaic/Mosaic.UI/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Mosaic.UI.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,12 @@ namespace Mosaic.UI
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		public RenderSettingsViewModel ViewModel { get; set; } = new RenderSettingsViewModel();
+
 		public MainWindow()
 		{
 			InitializeComponent();
+			DataContext = ViewModel;
 		}
 	}
 }
---
+++ src/ImageMosaic/Mosaic.UI/ViewModels/RenderSettingsViewModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mosaic.UI.ViewModels
+{
+	public class RenderSettingsViewModel : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public int _columns;
+		public int Columns 
+		{
+			get => _columns;
+			set
+			{
+				_columns = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+		public int _rows;
+		public int Rows
+		{
+			get => _rows;
+			set
+			{
+				_rows = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+		public int _resolution;
+		public int Resolution 
+		{
+			get => _resolution;
+			set
+			{
+				_resolution = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+		public int _contrast;
+		public int Contrast 
+		{
+			get => _contrast;
+			set
+			{
+				_contrast = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+		public int _brightness;
+		public int Brightness 
+		{
+			get => _brightness;
+			set
+			{
+				_brightness = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+		public int _red;
+		public int Red 
+		{
+			get => _red;
+			set
+			{
+				_red = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+		public int _green;
+		public int Green 
+		{
+			get => _green;
+			set
+			{
+				_green = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+		public int _blue;
+		public int Blue 
+		{
+			get => _blue;
+			set
+			{
+				_blue = value;
+				NotifyPropertyChanged();
+			}
+		}
+
+		private void NotifyPropertyChanged([CallerMemberName]string propertyName = "")
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
