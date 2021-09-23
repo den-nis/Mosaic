ec0030dc66ac38bc363f5ee0aad457d0fd6eadb7
Dennis <50838791+den-nis@users.noreply.github.com>
2021-09-23 22:13:34 +0200

    Updated unit tests

---
+++ src/ImageMosaic/Mosaic.Tests/ImageMock.cs
@@ -0,0 +1,118 @@
+﻿using ImageMagick;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mosaic.Graphics;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mosaic.Tests
+{
+	public class ImageMock : IDisposable
+	{
+		private MagickImage _internalImage;
+		private static Random _rng = new Random();
+
+		public static ImageMock Build(params string[] data)
+		{
+			var mock = new ImageMock();
+			mock.BuildImage(data);
+			return mock;
+		}
+
+		public static ImageMock Noise(int width, int height)
+		{
+			var mock = new ImageMock();
+			mock.BuildImageNoise(width, height);
+			return mock;
+		}
+
+		public void SetFromStream(Stream stream)
+		{
+			_internalImage = new MagickImage(stream);
+		}
+
+		public Stream GetMemoryStream()
+		{
+			MemoryStream stream = new();
+			_internalImage.Write(stream, MagickFormat.Png);
+			stream.Position = 0;
+
+			return stream;
+		}
+
+		private void BuildImageNoise(int width, int height)
+		{ 
+			MagickImage image = new MagickImage(MagickColors.Black, width, height);
+			using var pixels = image.GetPixels();
+
+			for (int y = 0; y < height; y++)
+			{
+				for (int x = 0; x < width; x++)
+				{
+					byte[] bytes = new byte[3];
+					_rng.NextBytes(bytes);
+					pixels.SetPixel(x, y, bytes);
+				}
+			}
+
+			_internalImage = image;
+		}
+
+		private void BuildImage(params string[] data)
+		{
+			MagickImage image = new MagickImage(MagickColors.Black, data[0].Length, data.Length);
+			using var pixels = image.GetPixels();
+
+			for (int y = 0; y < data.Length; y++)
+			{
+				for (int x = 0; x < data[0].Length; x++)
+				{
+					var c = ToColor(data[y][x]);
+					pixels.SetPixel(x, y, new[] { c.Red, c.Green, c.Blue });
+				}
+			}
+
+			_internalImage = image;
+		}
+
+		private Color ToColor(char c)
+		{
+			switch (c)
+			{
+				case 'W': return new Color(255, 255, 255);
+				case 'R': return new Color(255, 0, 0);
+				case 'G': return new Color(0, 255, 0);
+				case 'B': return new Color(0, 0, 255);
+				case ' ': return new Color(0, 0, 0);
+				default: throw new InvalidOperationException("Unknown color");
+			}
+		}
+
+		public void AssertEquals(ImageMock expected)
+		{
+			Assert.AreEqual(expected._internalImage.Width, _internalImage.Width, "Image width");
+			Assert.AreEqual(expected._internalImage.Height, _internalImage.Height, "Image height");
+
+			using var expectedPixels = expected._internalImage.GetPixels();
+			using var actualPixels = _internalImage.GetPixels();
+
+			for (int y = 0; y < _internalImage.Height; y++)
+			{
+				for (int x = 0; x < _internalImage.Width; x++)
+				{
+					Assert.AreEqual(expectedPixels[x, y][0], actualPixels[x, y][0], $"Incorrect red pixel at {x}x{y}");
+					Assert.AreEqual(expectedPixels[x, y][1], actualPixels[x, y][1], $"Incorrect green pixel {x}x{y}");
+					Assert.AreEqual(expectedPixels[x, y][2], actualPixels[x, y][2], $"Incorrect blue pixel {x}x{y}");
+				}
+			}
+		}
+
+		public void Dispose()
+		{
+			_internalImage?.Dispose();
+		}
+	}
+}
---
+++ src/ImageMosaic/Mosaic.Tests/MosaicImageTileTests.cs
@@ -0,0 +1,342 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mosaic.Graphics;
+using Mosaic.Settings;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mosaic.Tests
+{
+	[TestClass]
+	public class MosaicImageTileTests
+	{
+		[TestMethod]
+		public async Task TestMirror()
+		{
+			RenderSettings settings = new RenderSettings
+			{
+				Columns = 1,
+				Rows = 1,
+				UseMirror = true,
+				SamplesPerTile = 4,
+				Resolution = 2,
+			};
+
+			using ImageMock mainImage = ImageMock.Build(
+				" R",
+				"RR"
+			);
+
+			using ImageMock tileImage = ImageMock.Build(
+				"B ",
+				"BB"
+			);
+
+			using ImageMock expectedImage = ImageMock.Build(
+				" B",
+				"BB"
+			);
+
+			using ImageMock result = await QuickRender(tileImage, mainImage, settings);
+			result.AssertEquals(expectedImage);
+		}
+
+		[TestMethod]
+		public async Task TestMirrorAndRotation()
+		{
+			RenderSettings settings = new RenderSettings
+			{
+				Columns = 1,
+				Rows = 1,
+				UseMirror = true,
+				UseRotation = true,
+				SamplesPerTile = 9,
+				Resolution = 3,
+			};
+
+			using ImageMock mainImage = ImageMock.Build(
+				"RR ",
+				" R ",
+				" RR"
+			);
+
+			using ImageMock tileImage = ImageMock.Build(
+				"B  ",
+				"BBB",
+				"  B"
+			);
+
+			using ImageMock expectedImage = ImageMock.Build(
+				"BB ",
+				" B ",
+				" BB"
+			);
+
+			using ImageMock result = await QuickRender(tileImage, mainImage, settings);
+			result.AssertEquals(expectedImage);
+		}
+
+		[TestMethod]
+		public async Task TestRightRotation()
+		{
+			RenderSettings settings = new RenderSettings
+			{
+				Columns = 1,
+				Rows = 1,
+				UseRotation = true,
+				SamplesPerTile = 4,
+				Resolution = 2,
+			};
+
+			using ImageMock mainImage = ImageMock.Build(
+				" R",
+				" R"
+			);
+
+			using ImageMock tileImage = ImageMock.Build(
+				"BB",
+				"  "
+			);
+
+			using ImageMock expectedImage = ImageMock.Build(
+				" B",
+				" B"
+			);
+
+			using ImageMock result = await QuickRender(tileImage, mainImage, settings);
+			result.AssertEquals(expectedImage);
+		}
+
+		[TestMethod]
+		public async Task TestLeftRotation()
+		{
+			RenderSettings settings = new RenderSettings
+			{
+				Columns = 1,
+				Rows = 1,
+				UseRotation = true,
+				SamplesPerTile = 4,
+				Resolution = 2,
+			};
+
+			using ImageMock mainImage = ImageMock.Build(
+				"R ",
+				"R "
+			);
+
+			using ImageMock tileImage = ImageMock.Build(
+				"BB",
+				"  "
+			);
+
+			using ImageMock expectedImage = ImageMock.Build(
+				"B ",
+				"B "
+			);
+
+			using ImageMock result = await QuickRender(tileImage, mainImage, settings);
+			result.AssertEquals(expectedImage);
+		}
+
+		[TestMethod]
+		public async Task TestNoRotation()
+		{
+			RenderSettings settings = new RenderSettings
+			{
+				Columns = 1,
+				Rows = 1,
+				UseRotation = true,
+				SamplesPerTile = 4,
+				Resolution = 2,
+			};
+
+			using ImageMock mainImage = ImageMock.Build(
+				"R ",
+				"R "
+			);
+
+			using ImageMock tileImage = ImageMock.Build(
+				"B ",
+				"B "
+			);
+
+			using ImageMock expectedImage = ImageMock.Build(
+				"B ",
+				"B "
+			);
+
+			using ImageMock result = await QuickRender(tileImage, mainImage, settings);
+			result.AssertEquals(expectedImage);
+		}
+
+		[TestMethod]
+		public async Task TestComplexShapeRotation()
+		{
+			RenderSettings settings = new RenderSettings
+			{
+				Columns = 1,
+				Rows = 1,
+				UseRotation = true,
+				UseMirror = false,
+				SamplesPerTile = 9,
+				Resolution = 3,
+			};
+
+			using ImageMock mainImage = ImageMock.Build(
+				"R  ",
+				"RR ",
+				"RRR"
+			);
+
+			using ImageMock tileImage = ImageMock.Build(
+				" BB",
+				"  B",
+				"   "
+			);
+
+			using ImageMock expectedImage = ImageMock.Build(
+				"   ",
+				"B  ",
+				"BB "
+			);
+
+			using ImageMock result = await QuickRender(tileImage, mainImage, settings);
+			result.AssertEquals(expectedImage);
+		}
+
+		[TestMethod]
+		public async Task TestMultipleOverlap()
+		{
+			RenderSettings settings = new RenderSettings
+			{
+				Columns = 1,
+				Rows = 1,
+				UseRotation = true,
+				SamplesPerTile = 4,
+				Resolution = 2,
+			};
+
+			using ImageMock mainImage = ImageMock.Build(
+				"R ",
+				"RR"
+			);
+
+			using ImageMock tileImage = ImageMock.Build(
+				" B",
+				"BB"
+			);
+
+			using ImageMock expectedImage = ImageMock.Build(
+				"B ",
+				"BB"
+			);
+
+			using ImageMock result = await QuickRender(tileImage, mainImage, settings);
+			result.AssertEquals(expectedImage);
+		}
+
+		[TestMethod]
+		public async Task TestMultipleChoice()
+		{
+			RenderSettings settings = new RenderSettings
+			{
+				Columns = 1,
+				Rows = 1,
+				UseRotation = true,
+				SamplesPerTile = 4,
+				Resolution = 2,
+			};
+
+			using ImageMock mainImage = ImageMock.Build(
+				"R ",
+				"  "
+			);
+
+			using ImageMock tileImage = ImageMock.Build(
+				" B",
+				"B "
+			);
+
+			using ImageMock expectedImage = ImageMock.Build(
+				"B ",
+				" B"
+			);
+
+			using ImageMock result = await QuickRender(tileImage, mainImage, settings);
+			result.AssertEquals(expectedImage);
+		}
+
+		[TestMethod]
+		public async Task TestNearMatch()
+		{
+			RenderSettings settings = new RenderSettings
+			{
+				Columns = 1,
+				Rows = 1,
+				UseRotation = true,
+				SamplesPerTile = 4,
+				Resolution = 8,
+				UseAverageSamples = true,
+			};
+
+			using ImageMock mainImage = ImageMock.Build(
+				"R       ",
+				"        ",
+				"        ",
+				"        ",
+				"        ",
+				"        ",
+				"        ",
+				"        "
+			);
+
+			using ImageMock tileImage = ImageMock.Build(
+				"        ",
+				"        ",
+				"        ",
+				"        ",
+				"        ",
+				"        ",
+				"      R ",
+				"        "
+			);
+
+			using ImageMock expectedImage = ImageMock.Build(
+				"        ",
+				" R      ",
+				"        ",
+				"        ",
+				"        ",
+				"        ",
+				"        ",
+				"        "
+			);
+
+			using ImageMock result = await QuickRender(tileImage, mainImage, settings);
+			result.AssertEquals(expectedImage);
+		}
+
+		private async Task<ImageMock> QuickRender(ImageMock tile, ImageMock main, RenderSettings settings)
+		{
+			using var tileStream = tile.GetMemoryStream();
+			using var mainStream = main.GetMemoryStream();
+
+			TileSet sources = new TileSet(settings.Resolution, CropMode.Center);
+			await sources.LoadTilesAsync(new[] { new PictureSourceStream(tileStream) }, null);
+
+			MosaicImage mosaic = new MosaicImage(sources, settings);
+			await mosaic.SetMainImageAsync(new PictureSourceStream(mainStream, "Main"));
+
+			using MemoryStream result = new();
+			(await mosaic.RenderAsync()).Write(result);
+			result.Position = 0;
+
+			ImageMock mock = new();
+			mock.SetFromStream(result);
+			return mock;
+		}
+	}
+}
